3374e8387a8260a06654dfd137e0ab7ea2d058cc
MelSaFer <dhnmelfer@estudiantec.cr>
2022-11-24 18:09:58 -0600

    Primer avance de UI

---
+++ src/proyecto2lenguajes/Proyecto2Lenguajes.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2lenguajes;
+
+/**
+ *
+ * @author melan
+ */
+public class Proyecto2Lenguajes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

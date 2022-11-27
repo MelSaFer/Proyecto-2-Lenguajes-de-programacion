a849ff62244b19c4433cd9a253b4560b5ddfadd4
moisose <moises.solano@estudiantec.cr>
2022-11-26 23:04:35 -0600

    Agregar al final e inicio, estaEnLista, eliminarElemento, eliminar, listar, insertionSort

--- src/proyecto2lenguajes/Lista.java
+++ src/proyecto2lenguajes/Lista.java
@@ -27,24 +27,134 @@ public class Lista {
         this.nodoSiguiente = nodoSiguiente;
     }
     
-    public void agregarElemento( Elemento nuevoElemento) {
-        
+    // agrega el numero al final de la lista
+    public void agregarElemento(int valor_) {
+        Elemento nuevoElemento = new Elemento();
+        nuevoElemento.setValor(valor_);
+        // esta vacia la lista
+        if(nodoSiguiente == null){
+            nodoSiguiente = nuevoElemento;
+        }
+        else{
+            Elemento recorrer = nodoSiguiente;
+            while(recorrer.getNodoSiguiente() != null){
+                recorrer = recorrer.getNodoSiguiente();
+            }
+            recorrer.setNodoSiguiente(nuevoElemento);
+        }        
     }
     
-    public void eliminarElemento( Elemento nuevoElemento) {
-        
+    // agrega el numero al inicio de la lista
+    public void agregarElementoI(int valor_) {
+        Elemento nuevoElemento = new Elemento();
+        nuevoElemento.setValor(valor_);
+        nuevoElemento.setNodoSiguiente(nodoSiguiente);
+        nodoSiguiente = nuevoElemento;          
     }
     
+    // retorna true en caso de que se encuentre el numero en la lista
+    public boolean estaEnLista (int valor){
+        Elemento elementoAux = nodoSiguiente;
+        boolean esta = false;
+        while(elementoAux != null && !esta){
+            if(elementoAux.getValor() == valor){
+                esta = true;
+            }
+            else{
+                elementoAux = elementoAux.getNodoSiguiente();
+            }
+        }
+        return esta;
+    }
+    
+    // elimina la primera apacicion del elemento seleccionado
+    // retorna 1 en caso de que se borre
+    public int eliminarElemento(int valorEliminar) {
+        // la lista tiene elementos
+        if(nodoSiguiente != null){
+            
+            // el elemento esta en la lista
+            if(estaEnLista(valorEliminar)){
+                
+                // el primer elemento es el que se quiere eliminar
+                if(nodoSiguiente.getValor() == valorEliminar){
+                    nodoSiguiente = nodoSiguiente.getNodoSiguiente();
+                    return 1;
+                }
+                else{
+                    Elemento aux_borrar = nodoSiguiente; // copia de la lista
+                    Elemento aux = null; // se utiliza para eliminar el elemento
+
+                    // recorre los elementos de la lista hasta que el siguiente sea
+                    // el buscado
+                    while(aux_borrar.getNodoSiguiente().getValor() != valorEliminar){
+                        aux_borrar = aux_borrar.getNodoSiguiente();
+                    }
+
+                    aux = aux_borrar.getNodoSiguiente().getNodoSiguiente();
+                    aux_borrar.setNodoSiguiente(aux);
+                    return 1;
+                }
+            }
+        }
+        
+        return 0;
+    }
+    
+    // vacia la lista de numeros
     public void eliminar() {
-        
+        nodoSiguiente = null;
     }
     
     public void insertionSort() {
+
+        Elemento listaResultado = null;
+        Elemento elementoActual = nodoSiguiente;
+        Elemento siguiente = null;
+
+        while (elementoActual != null){
+
+            siguiente = elementoActual.nodoSiguiente;
+            
+            // la lista resultado es vacia o su cabeza es >= al elemento actual
+            if(listaResultado == null || listaResultado.getValor() >= elementoActual.getValor()){
+                elementoActual.setNodoSiguiente(listaResultado); // avanza en la lista el elemento actual
+                listaResultado = elementoActual;
+            }
+            else{
+                // lista auxiliar que se utiliza para insertar el elemento en la lista ordenada
+                Elemento aux = listaResultado; 
+                
+                // se busca la posicion en la que se tiene que insetar el elemento
+                while(aux.getNodoSiguiente() != null && aux.getNodoSiguiente().getValor() < elementoActual.getValor()){
+                    aux = aux.getNodoSiguiente();
+                }
+                // se inserta el elemento en el lugar correcto de forma ascendente
+                elementoActual.setNodoSiguiente(aux.getNodoSiguiente());
+                aux.setNodoSiguiente(elementoActual);
+            }
+            
+            // se avanza en la lista
+            elementoActual = siguiente;
+        }
+
+        // guardar el resultado
+        this.nodoSiguiente = listaResultado;
+    }
+    
+    public void binarySearch(int numeroBuscar) {
         
     }
     
-    public void binarySearch( int numeroBuscar) {
-        
+    // imprime los valores de la lista
+    public void listar(){
+        if (nodoSiguiente != null) {
+            Elemento aux = nodoSiguiente;
+            while(aux != null){
+                System.out.print(aux.getValor() + ", ");
+                aux = aux.getNodoSiguiente();
+            }
+        }
     }
     
 }
--- src/proyecto2lenguajes/main.java
+++ src/proyecto2lenguajes/main.java
@@ -19,9 +19,26 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         Lista listaActual = new Lista();
+        listaActual.agregarElemento(3);
+        listaActual.agregarElemento(4);
+        listaActual.agregarElemento(23);
+        listaActual.agregarElemento(2);
+        listaActual.agregarElemento(1);
+        listaActual.agregarElemento(50);
         
-        MainFrame ventanaPrincipal = new MainFrame(listaActual);
-        ventanaPrincipal.setVisible(true);
+        
+        //System.out.println(listaActual.estaEnLista(3));
+        listaActual.listar();
+        listaActual.insertionSort();
+        System.out.println("");
+        listaActual.listar();
+        
+        //System.out.println(listaActual.eliminarElemento(3));
+        //listaActual.listar();
+        //listaActual.eliminar();
+        //listaActual.listar();
+        //MainFrame ventanaPrincipal = new MainFrame(listaActual);
+        //ventanaPrincipal.setVisible(true);
   
         
     }
